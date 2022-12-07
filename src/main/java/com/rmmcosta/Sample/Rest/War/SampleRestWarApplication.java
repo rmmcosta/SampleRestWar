da840cf554866f82827b250064be6010a97220a6
Ricardo Costa <ricardocosta101085@gmail.com>
2022-12-07 22:56:46 +0000

    refactor:SampleRestWarApplication now extends directly the SpringBootServletInitializer

--- src/main/java/com/rmmcosta/Sample/Rest/War/SampleRestWarApplication.java
+++ src/main/java/com/rmmcosta/Sample/Rest/War/SampleRestWarApplication.java
@@ -2,12 +2,18 @@ package com.rmmcosta.Sample.Rest.War;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleRestWarApplication {
+public class SampleRestWarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleRestWarApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SampleRestWarApplication.class);
+	}
 }
